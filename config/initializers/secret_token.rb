2841573c04a81e163980af7daa269cd1640943d5
Jonathan Friedman <friedmanj98@gmail.com>
2012-07-17 18:38:03 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Insureon::Application.config.secret_token = 'b335bc2133005e4fc8ea97a904113304b85e8aa8d0b5f71a278592301843f06d7521aecd71b94c25b1c446981ace609fcbc1b826e7ef189072cfe447d6878903'
